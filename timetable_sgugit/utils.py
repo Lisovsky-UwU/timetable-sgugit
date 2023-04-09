da584c6ea0e56bf28659a9a7ea2f5a0016b12c50
Некит программист < @DESKTOP-0N9MUS7>
2023-04-09 21:38:14 +0700

    update create db function

--- timetable_sgugit/utils.py
+++ timetable_sgugit/utils.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 from .exceptions import DataBaseException
 from .orm import engine
 from .orm import Base
@@ -5,8 +7,8 @@ from .orm import Base
 
 def create_db():
     try:
-        print('Создание таблиц в БД...')
+        logger.info('Создание таблиц в БД...')
         Base.metadata.create_all(engine)
-        print('Таблицы созданы.')
+        logger.success('Таблицы созданы.')
     except Exception as e:
         raise DataBaseException(f'Ошибка создания таблиц: {e}')
