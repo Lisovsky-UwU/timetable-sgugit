eb120e1bfc08d6f295ccd083886f1846ba633fbf
Некит программист < @DESKTOP-0N9MUS7>
2023-04-09 21:41:25 +0700

    upd group orm model

--- timetable_sgugit/orm/group.py
+++ timetable_sgugit/orm/group.py
@@ -10,7 +10,11 @@ class Group(Base):
 
     __tablename__ = 'groups'
 
-    id         = Column(Integer, primary_key=True, autoincrement=True)
-    name       = Column(String, nullable=False)
+    id             = Column(Integer, primary_key=True, autoincrement=True)
+    sgugit_id      = Column(Integer, nullable=False)
+    name           = Column(String, nullable=False, index=True)
+    course         = Column(Integer, nullable=False, index=True)
+    institute      = Column(Integer, nullable=False, index=True)
+    education_form = Column(Integer, nullable=False, index=True)
 
-    lessons_db = relationship('Lesson', back_populates='group_db', uselist=True)
+    lessons_db     = relationship('Lesson', back_populates='group_db', uselist=True)
