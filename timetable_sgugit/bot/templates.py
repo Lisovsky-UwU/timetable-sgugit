d24b34aab126a01d78ac1b2d0c48cbd3af1a41f6
Некит программист < @DESKTOP-0N9MUS7>
2023-04-11 01:09:28 +0700

    upd templates

--- timetable_sgugit/bot/templates.py
+++ timetable_sgugit/bot/templates.py
@@ -3,6 +3,11 @@ BTN_SELECT_TEACHER = 'Преподаватели'
 BTN_SELECT_AUDIENCE = 'Аудитории'
 BTN_SELECT_FAVORITE = 'Избранное'
 BTN_SELECT_FEEDBACK = 'Обратная связь'
+BTN_OPEN_CALENDAR = 'Выбрать день'
+BTN_NEXT_DAY = 'След. день'
+BTN_PREV_DAY = 'Пред. день'
+BTN_NEXT_MONTH = 'След. месяц'
+BTN_PREV_MONTH = 'Пред. месяц'
 
 BTN_BACK = 'Назад'
 
@@ -10,3 +15,23 @@ BTN_BACK = 'Назад'
 MESSAGE_MAIN_MENU = 'Выберите раздел'
 MESSAGE_SELECT_INSTITUTE = 'Выберите институт'
 MESSAGE_SELECT_FORM = 'Выберите тип обучения'
+MESSAGE_SELECT_COURSE = 'Выберите курс'
+MESSAGE_SELECT_GROUP = 'Выберите группу'
+MESSAGE_SELECT_DAY = 'Выберите день'
+
+MESSAGE_GROUP_LESSON_LIST = '''
+Группа {}
+Дата {}
+{} неделя
+{}
+'''
+
+NO_LESSON_ON_DAY = '\n❌ На выбранный день пар нет'
+
+LESSON_GROUP_INFO = '''
+{}. {}
+📚 {}
+👨‍🏫 {}
+🎓 {}
+🏢 {}
+'''
