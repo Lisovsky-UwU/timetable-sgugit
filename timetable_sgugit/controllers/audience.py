f04e13f4437c1e4012a4a91e1b127e3bc248299f
Некит программист < @DESKTOP-0N9MUS7>
2023-04-14 18:09:03 +0700

    update audience controller

--- timetable_sgugit/controllers/audience.py
+++ timetable_sgugit/controllers/audience.py
@@ -1,7 +1,10 @@
 from typing import Type
 from typing import List
+from typing import Iterable
+from functools import lru_cache
 
 from ..services import AudienceDBService
+from ..models import AudienceCreateRequest
 from ..orm import Audience
 
 
@@ -14,3 +17,33 @@ class AudienceDBController:
     def get_all(self) -> List[Audience]:
         with self.service_type() as service:
             return service.get_all()
+
+
+    @lru_cache(maxsize=None)
+    def create_if_not_exists(self, name: str) -> Audience:
+        with self.service_type() as service:
+            data = service.get_by_name(name)
+            if data is None:
+                data = service.create(
+                    Audience(
+                        name     = name,
+                        building = 0,
+                    )
+                )
+
+            return data
+
+
+    def fill_for_iter(self, payload: Iterable[AudienceCreateRequest]) -> List[Audience]:
+        with self.service_type() as service:
+            service.delete_for_list(service.get_all())
+            data = service.create_for_iter(
+                Audience(
+                    name     = data.name,
+                    building = data.building,
+                )
+                for data in payload
+            )
+            service.commit()
+
+            return data
